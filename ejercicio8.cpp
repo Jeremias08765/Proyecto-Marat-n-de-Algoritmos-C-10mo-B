c0c176cd0938716f84975cb1d7eaab44be7aa280
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-06 13:16:19 -0500

    Update ejercicio8.cpp

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Ingrese dos numeros: ";
+    cin >> a >> b;
+    cout << "El producto es: " << a * b;
+    return 0;
+}
