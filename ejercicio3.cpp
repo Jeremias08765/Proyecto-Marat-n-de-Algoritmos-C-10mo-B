c0e2ac46acb57313ac8ffaaf9dce60f4f14c78dc
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-06 13:14:46 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    cout << "El doble es: " << num * 2;
+    return 0;
+}
