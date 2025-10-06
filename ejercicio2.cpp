2d6b0ef9ebd2b74db1829ab82b7056e272257393
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-06 13:14:30 -0500

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Ingrese dos numeros: ";
+    cin >> a >> b;
+    cout << "Los numeros son: " << a << " y " << b;
+    return 0;
+}
