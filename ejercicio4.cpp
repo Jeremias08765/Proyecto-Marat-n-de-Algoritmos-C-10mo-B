45c864627facefb8213a6c029ef196d12fddf6a4
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-06 13:15:04 -0500

    Update ejercicio4.cpp

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    cout << "El triple es: " << num * 3;
+    return 0;
+}
 
