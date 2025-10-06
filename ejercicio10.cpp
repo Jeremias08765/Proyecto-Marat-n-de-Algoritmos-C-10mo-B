7f0648582f4e4aeb07ce70790e47ed69f60c4db0
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-06 13:17:04 -0500

    Update ejercicio10.cpp

--- ejercicio10.cpp
+++ ejercicio10.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cout << "Ingrese tres numeros: ";
+    cin >> a >> b >> c;
+    cout << "Los numeros son: " << a << ", " << b << ", " << c;
+    return 0;
+}
