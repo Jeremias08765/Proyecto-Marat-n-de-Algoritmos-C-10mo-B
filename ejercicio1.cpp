f16177e232e8691c8087ef1e30c5f1dc647230fc
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-06 13:13:05 -0500

    Update ejercicio1.cpp

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,1 +1,11 @@
-
+Ejercicio 1: Leer un número y mostrarlo
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    cout << "El numero ingresado es: " << num;
+    return 0;
+}
+cpp
