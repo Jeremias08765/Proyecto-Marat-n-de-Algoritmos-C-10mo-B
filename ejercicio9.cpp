468ab32f9bd4e373cd13716288660baaba309d27
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-06 13:16:38 -0500

    Update ejercicio9.cpp

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    float a, b;
+    cout << "Ingrese dos numeros: ";
+    cin >> a >> b;
+    cout << "La division es: " << a / b;
+    return 0;
+}
