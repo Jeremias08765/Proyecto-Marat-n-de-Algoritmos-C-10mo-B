cc51d79fcf9a9a8262ce76e293a4e11c29bd21fc
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-06 13:15:17 -0500

    Update ejercicio5.cpp

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    float num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    cout << "La mitad es: " << num / 2;
+    return 0;
+}
