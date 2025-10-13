bce3e93ff0454f0eec6c6905ca75e0fd786e0b35
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:20:11 -0500

    Update ejercicio60.cpp

--- ejercicio60.cpp
+++ ejercicio60.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    float num, suma=0;
+    cout << "Cuantos numeros: ";
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        cout << "Numero " << i << ": ";
+        cin >> num;
+        suma += num;
+    }
+    cout << "Promedio: " << suma/n;
+    return 0;
+}
