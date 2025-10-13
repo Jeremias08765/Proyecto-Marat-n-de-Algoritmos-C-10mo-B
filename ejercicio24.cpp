9c2e2a11752b3f09d32bab3eae43acf79cae322d
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:16:50 -0500

    Update ejercicio24.cpp

--- ejercicio24.cpp
+++ ejercicio24.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    float r;
+    cout << "Radio: ";
+    cin >> r;
+    cout << "Perimetro: " << 2 * M_PI * r;
+    return 0;
+}
