c061c7e5ee1ec0ac020fdd2a0ae1aa8c85c69fd1
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:19:04 -0500

    Update ejercicio47.cpp

--- ejercicio47.cpp
+++ ejercicio47.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int horas;
+    float pago;
+    cout << "Horas trabajadas: ";
+    cin >> horas;
+    if (horas <= 40) pago = horas * 10;
+    else pago = 40 * 10 + (horas - 40) * 15;
+    cout << "Pago total: " << pago;
+    return 0;
+}
