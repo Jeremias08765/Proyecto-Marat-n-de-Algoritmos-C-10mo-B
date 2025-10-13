13769e8df485f6d37dc855696d366471b7d00fa2
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:18:49 -0500

    Update ejercicio40.cpp

--- ejercicio40.cpp
+++ ejercicio40.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cout << "Ingrese tres numeros: ";
+    cin >> a >> b >> c;
+    if (a >= b && a >= c) cout << "Mayor: " << a;
+    else if (b >= a && b >= c) cout << "Mayor: " << b;
+    else cout << "Mayor: " << c;
+    return 0;
+}
