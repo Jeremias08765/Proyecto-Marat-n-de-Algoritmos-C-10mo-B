6368d472ff66b50fad05eba7cbb1e59a438f38fb
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:16:41 -0500

    Update ejercicio23.cpp

--- ejercicio23.cpp
+++ ejercicio23.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    float r;
+    cout << "Radio: ";
+    cin >> r;
+    cout << "Area: " << M_PI * r * r;
+    return 0;
+}
