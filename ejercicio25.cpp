2a0da9cd05e24be5243bd4ee91bf670d1fcaa057
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:17:01 -0500

    Update ejercicio25.cpp

--- ejercicio25.cpp
+++ ejercicio25.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    float b1,b2,h;
+    cout << "Base mayor, base menor y altura: ";
+    cin >> b1 >> b2 >> h;
+    cout << "Area: " << ((b1+b2)*h)/2;
+    return 0;
+}
