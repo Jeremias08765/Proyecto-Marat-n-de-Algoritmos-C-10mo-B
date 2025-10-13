3f40c0468c218294704fd618dd42fe5fc541309f
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:18:35 -0500

    Update ejercicio31.cpp

--- ejercicio31.cpp
+++ ejercicio31.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Numero: ";
+    cin >> n;
+    if (n >= 0) cout << "Positivo";
+    else cout << "Negativo";
+    return 0;
+}
 
