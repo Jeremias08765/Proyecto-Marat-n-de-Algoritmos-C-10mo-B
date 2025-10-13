9e9b740eefceb3b76091d5e6e6d3678a07323f6f
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:25:15 -0500

    Update ejercicio127.cpp

--- ejercicio127.cpp
+++ ejercicio127.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+void linea(int n) {
+    for (int i = 0; i < n; i++)
+        cout << "-";
+}
+int main() {
+    int n;
+    cin >> n;
+    linea(n);
+    return 0;
+}
