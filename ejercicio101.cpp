b2e1856c90032b6cfb74f4b53853caad1737bd6e
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:25:29 -0500

    Update ejercicio101.cpp

--- ejercicio101.cpp
+++ ejercicio101.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+void convertirTiempo(int s) {
+    int h = s / 3600;
+    int m = (s % 3600) / 60;
+    int seg = s % 60;
+    cout << h << "h " << m << "m " << seg << "s";
+}
+int main() {
+    int s;
+    cin >> s;
+    convertirTiempo(s);
+    return 0;
+}
