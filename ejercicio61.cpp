828b99e17631bc444c3b8b1a56cb86dd183b9147
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:20:26 -0500

    Update ejercicio61.cpp

--- ejercicio61.cpp
+++ ejercicio61.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n, num, cont=0;
+    cout << "Cantidad de numeros: ";
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        cout << "Numero " << i << ": ";
+        cin >> num;
+        if(num > 0) cont++;
+    }
+    cout << "Positivos: " << cont;
+    return 0;
+}
