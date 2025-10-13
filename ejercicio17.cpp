78627eadc20a8d11e573582c04afbe9cd253751a
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:15:40 -0500

    Update ejercicio17.cpp

--- ejercicio17.cpp
+++ ejercicio17.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    float a,b,c;
+    cout << "Ingrese tres numeros: ";
+    cin >> a >> b >> c;
+    cout << "Promedio: " << (a+b+c)/3;
+    return 0;
+}
