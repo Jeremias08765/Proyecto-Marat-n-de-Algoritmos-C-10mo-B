4e453b9442ffde9b18fa9bc217448894cfb79bbf
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:23:16 -0500

    Update ejercicio132.cpp

--- ejercicio132.cpp
+++ ejercicio132.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int c2=0,c3=0;
+    for(int i=1;i<=50;i++){
+        if(i%2==0) c2++;
+        if(i%3==0) c3++;
+    }
+    cout << "Multiplos de 2: " << c2 << ", Multiplos de 3: " << c3;
+    return 0;
+}
