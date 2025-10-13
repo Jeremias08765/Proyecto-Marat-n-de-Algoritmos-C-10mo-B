9e9e05840b9bd565a8acd2169aabe008805f38f6
Jeremias08765 <jgutierrez@alumno.montepiedra.edu.ec>
2025-10-13 13:18:06 -0500

    Update ejercicio30.cpp

--- ejercicio30.cpp
+++ ejercicio30.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    float D,d;
+    cout << "Diagonal mayor y menor: ";
+    cin >> D >> d;
+    cout << "Area: " << (D*d)/2;
+    return 0;
+}
